8fcab6adace26a214bb038edebcf1b426fa335e4
AntarleenChand05 <antarleen.chand.btech2024@sitpune.edu.in>
2025-08-07 00:06:29 +0530

    Create matrix_multiplication.cpp

---
+++ matrix_multiplication.cpp
@@ -0,0 +1,85 @@
+// Antarleen Chand 
+// 24070123018 | ENTC A1
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr1[100][100], rows1, columns1;
+    cout<<"For the 1st matrix:\n";
+    cout << "Enter the number of rows: ";
+    cin >> rows1;
+    cout << "Enter the number of columns: ";
+    cin >> columns1;
+
+    int arr2[100][100], rows2, columns2;
+    cout<<"For the 2nd matrix:\n";
+    cout << "Enter the number of rows: ";
+    cin >> rows2;
+    cout << "Enter the number of columns: ";
+    cin >> columns2;
+    
+    if (columns1 != rows2){
+        cout << "Multiplication not possible. Columns of 1st matrix must equal rows of 2nd matrix." << endl;
+        return 1;
+    }
+
+    int result[100][100] = {0};
+    cout << "Enter elements of the 1st matrix (row-wise):" << endl;
+    for (int i = 0; i < rows1; i++){
+        for (int j = 0; j < columns1; j++){
+            cin >> arr1[i][j];
+        }
+    cout<<endl;
+    }
+
+    cout << "Enter elements of the 2nd matrix (row-wise):" << endl;
+    for (int i = 0; i < rows2; i++){
+        for (int j = 0; j < columns2; j++){
+            cin >> arr2[i][j];
+        }   
+    cout<<endl;
+    }
+
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < columns2; j++) {
+            for (int k = 0; k < columns1; k++) {
+                result[i][j] += arr1[i][k] * arr2[k][j];
+            }
+        }
+    }
+    cout << "The resulting matrix after multiplication is:" << endl;
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < columns2; j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+
+
+    return 0;
+}
+
+/* OUTPUT : 
+For the 1st matrix:
+Enter the number of rows: 2
+Enter the number of columns: 3
+For the 2nd matrix:
+Enter the number of rows: 3
+Enter the number of columns: 2
+Enter elements of the 1st matrix (row-wise):
+4 8 16
+
+3 9 27 
+
+Enter elements of the 2nd matrix (row-wise):
+5 12 20
+
+7 15 30
+
+
+The resulting matrix after multiplication is:
+420 584 
+600 909 
+*/
